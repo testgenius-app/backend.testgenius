b9e35222a2b8e187624fb765f20acc9446650c16
Durbek Saydaliyev <saydaliyevdurbek0512@gmail.com>
2025-05-11 12:54:55 +0500

    Implement DownloadDto for file download functionality; add downloadTest method in TestService and corresponding endpoint in TestController.

--- src/modules/test/test.controller.ts
+++ src/modules/test/test.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/auth.guard';
 import { TestService } from './test.service';
@@ -6,19 +6,37 @@ import { FilterDto } from './dto/filter.dto';
 import { IUser } from 'src/core/types/iuser.type';
 import { User } from 'src/common/decorators/user.decorator';
 import { Test } from '@prisma/client';
+import { CreateTestDto } from './dto/create-test.dto';
+import { DownloadDto } from './dto/download.dto';
 
 @ApiTags('Test')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth('access-token')
 @Controller({ path: 'test', version: '1' })
 export class TestController {
   constructor(private readonly testService: TestService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth('access-token')
   async getTestsByOwnerId(
     @User() user: IUser,
     @Query() filterDto: FilterDto,
   ): Promise<{ tests: Test[]; count: number }> {
     return this.testService.getTestsByOwnerId(user, filterDto);
+  }
+
+  @Post()
+  async createTest(
+    @User() user: IUser,
+    @Body() body: CreateTestDto,
+  ): Promise<any> {
+    return this.testService.create(user, body);
+  }
+
+  @Post('download')
+  async downloadTest(
+    @User() user: IUser,
+    @Query() query: DownloadDto,
+  ): Promise<any> {
+    return this.testService.downloadTest(user, query);
   }
 }
--- src/modules/test/test.service.ts
+++ src/modules/test/test.service.ts
@@ -5,9 +5,11 @@ import {
 } from '@nestjs/common';
 import { CreateTestDto } from './dto/create-test.dto';
 import { IUser } from 'src/core/types/iuser.type';
+import * as fs from 'fs';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 import { TestRepository } from './test.repository';
 import { FilterDto } from './dto/filter.dto';
+import { DownloadDto } from './dto/download.dto';
 
 @Injectable()
 export class TestService {
@@ -34,6 +36,22 @@ export class TestService {
       filterDto,
     );
     return { tests, count };
+  }
+  async downloadTest(user: IUser, query: DownloadDto) {
+    const { testId, type } = query;
+    const test = await this.testRepository.getTestById(testId);
+    if (!test || test.ownerId !== user.id) {
+      throw new NotFoundException('Test not found');
+    }
+    const file = fs.writeFileSync(
+      `${testId}.${type}`,
+      JSON.stringify(test),
+      'utf8',
+    );
+    return {
+      file,
+      message: 'File downloaded successfully',
+    };
   }
 
   async validateUser(user: IUser) {
